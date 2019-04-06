1d7db8c3d37ca14e6314678e458463d6298e957c
yuri1987 <yuri1987@gmail.com>
2019-04-06 17:17:11 +0300

    ttv links modified

--- scrapers.py
+++ scrapers.py
@@ -95,7 +95,7 @@ def get_ttv():
     xbmcplugin.endOfDirectory(addon_handle)
 
 def ttv_sport():
-    base_url = 'http://91.92.66.82/trash/ttv-list/ttv.sport.player.m3u'
+    base_url = 'http://91.92.66.82/trash/ttv-list/as.sport.player.m3u'
     source = read_url(base_url)
     if source:
         match= re.compile('#EXTINF:-1,(.+?)\n(.*)').findall(source)
@@ -143,7 +143,7 @@ def open_ttv_stream(url,name):
 
 
 def get_ttv_cat(cat,tag):
-    url="http://91.92.66.82/trash/ttv-list/ttv.m3u"
+    url="http://91.92.66.82/trash/ttv-list/ace.all.player.m3u"
     html=read_url(url)
     dicty=json.loads(tag)
     dicty = {k.encode('utf-8'): v for k, v in dicty.items()}
@@ -160,7 +160,7 @@ def get_ttv_cat(cat,tag):
 
 def ttv_cats():
     dict_torrent = {}
-    url="http://91.92.66.82/trash/ttv-list/ttv.m3u"
+    url="http://91.92.66.82/trash/ttv-list/ace.all.player.m3u"
     html_source=read_url(url)
     match = re.compile('#EXTINF:-1,(.+?)\n(.*)').findall(html_source)
     for title, acehash in match:
